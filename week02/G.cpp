2c25e645c884316e42e4591c5dec3138a55bebce
xenonnachille <116821026+xenonnachille@users.noreply.github.com>
2023-02-12 23:35:00 +0300

    Update G.cpp

--- week02/G.cpp
+++ week02/G.cpp
@@ -5,7 +5,7 @@ using std::cin;
 using std::endl;
 
 void is_palindrom(int n) {
-    int helping_num = n, reversed_num = 0;
+    long long int helping_num = n, reversed_num = 0;
     while (helping_num > 0) {
         reversed_num = 10 * reversed_num + helping_num % 10;
         helping_num /= 10;
